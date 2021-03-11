2963dae984f968cf840d5d94137a88c3b146f95f
Eric Rodriguez <ericrodriguez4@csus.edu>
2021-03-11 00:03:12 -0800

    Updated JUnit testing to include all coverage of the GiftCard class

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -42,4 +42,46 @@ public class GiftCardTest
 		card = new GiftCard(issuingStore, balance);
 		assertEquals("deduct()", "Remaining Balance:  90.00", card.deduct(10));
 	}
+	
+	@Test
+	public void deduct_NegAmount()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("deduct()", "Invalid Transaction", card.deduct(-10));
+	}
+	
+	@Test
+	public void deduct_NegBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("deduct()", "Amount Due:  10.00", card.deduct(110));
+	}
+	
+	@Test
+	public void constructor_OpeningBalance_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+	}
+	
+//	@Test
+//	public void constructor_IncorrectID_High()
+//	{
+//		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100.00);});
+//	}
 }
